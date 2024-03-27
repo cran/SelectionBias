7519b753055270ff98972ca04c8f68d33bed01e2
Stina Zetterstrom <stina.zetterstrom@statistics.uu.se>
2024-03-27 13:00:05 +0000

    version 2.0.0

--- inst/doc/SelectionBias.R
+++ inst/doc/SelectionBias.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -75,7 +75,23 @@ table1::table1(~ mic_ceph + urban + SES | zika, data=zika_learner2, render.conti
        caption = "Table 4. Proportions for the simulated dataset, by treatment status and overall, after both selections.")
 
 ## ----eval = TRUE--------------------------------------------------------------
-SVboundparametersM(whichEst = "RR_sub",
+# SV bound
+sensitivityparametersM(whichEst = "RR_tot",
+                   whichBound = "SV",
+                   Vval = matrix(c(1, 0, 0.85, 0.15), ncol = 2),
+                   Uval = matrix(c(1, 0, 0.5, 0.5), ncol = 2),
+                   Tcoef = c(-6.2, 1.75),
+                   Ycoef = c(-5.2, 5.0, -1.0),
+                   Scoef = matrix(c(1.2, 2.2, 0.0, 0.5,
+                                    2.0, -2.75, -4.0, 0.0),
+                                  ncol = 4),
+                   Mmodel = "L",
+                   pY1_T1_S1 = 0.286,
+                   pY1_T0_S1 = 0.004)
+
+# GAF bound
+sensitivityparametersM(whichEst = "RR_tot",
+                   whichBound = "GAF",
                    Vval = matrix(c(1, 0, 0.85, 0.15), ncol = 2),
                    Uval = matrix(c(1, 0, 0.5, 0.5), ncol = 2),
                    Tcoef = c(-6.2, 1.75),
@@ -88,30 +104,45 @@ SVboundparametersM(whichEst = "RR_sub",
                    pY1_T0_S1 = 0.004)
 
 ## ----eval = TRUE--------------------------------------------------------------
-SVbound(whichEst = "RR_sub",
-        RR_UY_S1 = 2.71,
-        RR_TU_S1 = 2.33)
+SVbound(whichEst = "RR_tot",
+        pY1_T1_S1 = 0.004,
+        pY1_T0_S1 = 0.286,
+        RR_UY_T1 = 2.71,
+        RR_UY_T0 = 1.94,
+        RR_SU_T1 = 1.80,
+        RR_SU_T0 = 2.00)
+
+## ----eval = TRUE--------------------------------------------------------------
+SVboundsharp(BF_U = 1.56,
+             pY1_T0_S1 = 0.27)
 
 ## ----eval = TRUE--------------------------------------------------------------
 attach(zika_learner)
 
-AFbound(whichEst = "RR_sub",
-        outcome = mic_ceph,
-        treatment = 1 - zika,
-        selection = sel_ind)
-
-## ----eval = TRUE--------------------------------------------------------------
-mean(sel_ind)
-
-## ----eval = TRUE--------------------------------------------------------------
-AFbound(whichEst = "RR_sub",
+AFbound(whichEst = "RR_tot",
         outcome = mic_ceph[sel_ind == 1],
-        treatment = 1 - zika[sel_ind == 1], 
+        treatment = zika[sel_ind == 1],
         selection = mean(sel_ind))
 
 ## ----eval = TRUE--------------------------------------------------------------
-SVboundsharp(BF_U = 1.56,
-             pY1_T0_S1 = 0.27,
-             SVbound = 1.56,
-             AFbound = 3.5)
+AFbound(whichEst = "RR_tot",
+        outcome = c(0.286, 0.004),
+        treatment = c(0.002, 0.998),
+        selection = mean(sel_ind))
+
+## ----eval = TRUE--------------------------------------------------------------
+GAFbound(whichEst = "RR_tot",
+         M = 0.4502,
+         m = 0.002, 
+         outcome = mic_ceph[sel_ind == 1],
+         treatment = zika[sel_ind == 1],
+         selection = mean(sel_ind))
+
+## ----eval = TRUE--------------------------------------------------------------
+CAFbound(whichEst = "RR_tot",
+         M = 0.3,
+         m = 0.005, 
+         outcome = c(0.286, 0.004),
+         treatment = c(0.002, 0.998),
+         selection = mean(sel_ind))
 
